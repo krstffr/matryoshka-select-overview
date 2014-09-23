32c0fc6dd275daa030b9c617aa8c70c0326cbdbe
Kristoffer Klintberg <krstffr@gmail.com>
2014-09-23 09:25:24 +0200

    Added a reset button for inputs.

--- views/matryoshka__customField__selectOverview.js
+++ views/matryoshka__customField__selectOverview.js
@@ -6,8 +6,13 @@ Template[tmplName].helpers({
 });
 
 Template[tmplName].events({
-	'click .matryoshka__nestable__container__button': function ( e, tmpl ) {
+	'click .matryoshka__customField__selectOverview__select-button': function ( e, tmpl ) {
 		var input = tmpl.find('.matryoshka__customField__selectOverview__locked-input');
 		MatryoshkaSelectOverview.popup.show( tmpl.data, $(input) );
+	},
+	'click .matryoshka__customField__selectOverview__reset-button': function ( e, tmpl ) {
+		// Find the input, reset it's value, blur() it to store it's value in Matryoshka
+		var input = tmpl.find('.matryoshka__customField__selectOverview__locked-input');
+		$(input).val('').focus().blur();
 	}
 });
